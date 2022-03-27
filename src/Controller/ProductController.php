573ef98aba7c40618f5e2e707d4fc4bd2e431657
AlbanW <alban45190@gmail.com>
2022-03-27 15:07:32 +0200

    product page & product show

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/product')]
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/show/{id}', name: 'app_product_show')]
+    public function index(int $id, ProduitRepository $pr): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $produit = $pr->find($id);
+        if($produit instanceof Produit)
+        {
+            return $this->render('/product/show.html.twig', [
+                'produit' => $produit
+            ]);
+        } else {
+            $this->addFlash("danger", "Le produit recherché n'existe pas.");
+            return $this->redirectToRoute('app_home');
+        }
     }
 }
